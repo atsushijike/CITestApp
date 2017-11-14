cd48b8f3a67a1e1fc92687a785eed84a253609ee
Atsushi Jike <a_jike@yumemi.co.jp>
2017-11-14 17:07:13 +0900

    Initial Commit

---
+++ CITestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CITestApp
+//
+//  Created by 寺家 篤史 on 2017/11/14.
+//  Copyright © 2017年 Yumemi Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
