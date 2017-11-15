65296053b1b3a3e971c467c57736bd476cf2a810
Atsushi Jike <a_jike@yumemi.co.jp>
2017-11-15 18:41:54 +0900

    Removed func applicationDidBecomeActive()

--- CITestApp/AppDelegate.swift
+++ CITestApp/AppDelegate.swift
@@ -21,9 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-    }
-
     func applicationWillTerminate(_ application: UIApplication) {
     }
 
